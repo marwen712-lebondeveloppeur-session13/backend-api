118fa7202ee559fa80d76b195dce0c3d1b0df363
Wassim Assidi <wassim.assidi@astrolab-group.com>
2025-04-21 18:45:08 +0100

    fix

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -12,7 +12,7 @@ export const getProducts = async (req, res) => {
 };
 
 export const createProduct = async (req, res) => {
-	const product = req.body; // user will send this data
+	const product = req.body; // user will send this data.
 
 	if (!product.name || !product.price || !product.image) {
 		return res.status(400).json({ success: false, message: "Please provide all fields" });
