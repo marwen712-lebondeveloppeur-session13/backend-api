a4f57b3d256e8c726a4dfa3594dc5f7784fe1def
Talel Ben Belgacem <talelbenbelgacem12@gmail.com>
2025-08-02 21:41:56 +0100

    session13 init

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("Hello Le bon developpeur Community ! - new feature 🚀");
+  res.send("Hello Le bon developpeur Community ! 🚀");
 });
 
 app.get("/health", async (req, res) => {
